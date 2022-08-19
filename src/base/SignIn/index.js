26849b5fe9ca66a2d509786c02c7804b3bb8863e
JYP <735269650@qq.com>
2022-08-19 15:48:54 +0800

    2022.8.19 暴露配置文件

---
+++ src/base/SignIn/index.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Index extends Component {
+    render() {
+        return (
+            <div>
+                1465465
+            </div>
+        );
+    }
+}
+
+export default Index;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react';  //导入规范
+// import ReactDOM from 'react-dom';
+// ReactDOM.render(
+//     <SingIn></SingIn>
+//     , document.querySelector('#root'))
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import {createRoot} from 'react-dom/client';    //1.8以后用这个
+import SingIn from './base/SignIn'
+
+const container = document.getElementById('root');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<SingIn tab="home"/>);
+
